821a04492144bba8fecb037a0e802a033aec8edd
himanshu964 <himanshu.jha.963@gmail.com>
2023-12-30 09:06:50 -0800

    done

--- src/Components/movieCard/MovieCard.jsx
+++ src/Components/movieCard/MovieCard.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 
 import "./MovieCard.scss";
 import Img from "../LazyLoadImage/Img";
-import CircleRating from "../circleRating/CircleRating";
+import CircleRating from "../CircleRating/CircleRating";
 import Genres from "../Genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
